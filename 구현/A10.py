956d08a1631f8782f648a51263cab741f2ef1d03
501501 <76206286+501501@users.noreply.github.com>
2021-07-06 12:06:39 +0900

    Create A10.py

---
+++ 구현/A10.py
@@ -0,0 +1,65 @@
+def rotate_a_matrix_by_90_degree(a):
+    n = len(a)  # 행 길이 계산
+    m = len(a[0])  # 열 길이 계산
+    result = [[0] * n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            result[j][n - i - 1] = a[i][j]
+    return result
+
+
+'''
+# 직접 구현한 rotate 함수
+def rotate(key_index):
+    rotate_key_index = [[0] * 2 for _ in range(len(key_index))]
+    # (x, y) -> (y, x)
+    for i in range(len(key_index)):
+        for j in range(2):
+            rotate_key_index[i][j] = key_index[i][1 - j]
+
+    # (y, x) -> (y, len(key_index)-1-x)
+    for i in range(len(key_index)):
+        for j in range(2):
+            if j == 1:
+                rotate_key_index[i][j] = len(key_index) - 1 - rotate_key_index[i][j]
+    return rotate_key_index
+'''
+
+
+def check(new_lock):
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length * 2):
+        for j in range(lock_length, lock_length * 2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+
+
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+
+    # 자물쇠의 크기를 기존의 3배로 변환
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+    # 새로운 자물쇠의 중앙 부분에 기존의 자물쇠 넣기
+    for i in range(n):
+        for j in range(n):
+            new_lock[i + n][j + n] = lock[i][j]
+
+    # 4가지 방향에 대해서 확인
+    for rotation in range(4):
+        key = rotate_a_matrix_by_90_degree(key)  # 열쇠 회전
+        for x in range(n * 2):
+            for y in range(n * 2):
+                # 자물쇠에 열쇠를 끼워넣기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                # 새로운 자물쇠에 열쇠가 정확히 들어맞는지 검사
+                if check(new_lock) == True:
+                    return True
+                # 자물쇠에서 열쇠를 다시 빼기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
+    return False
